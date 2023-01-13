cc77761c2503b36667b82d21be596d32a23a4dbd
Marvin Loreto <marvinjloreto@gmail.com>
2023-01-13 23:56:21 +0000

    Added items to favorites and styled favorites list: issue-2

--- js/main.js
+++ js/main.js
@@ -6,12 +6,16 @@ var ul = document.querySelector('.results-list');
 var detailsPage = document.querySelector('.details-container');
 var buttonSearch = document.querySelector('.button-search');
 var titleLink = document.querySelector('.nav-home');
+var favoritesPage = document.querySelector('.favorites-container');
+var favoriteLink = document.querySelector('.nav-favorite');
+var buttonFav = document.querySelector('.add-favorite');
 
 buttonSearch.addEventListener('click', handleSearch);
 function handleSearch(event) {
   resultsPage.classList.remove('hidden');
   searchPage.classList.add('hidden');
   detailsPage.classList.add('hidden');
+  favoritesPage.classList.add('hidden');
   data.view = 'results-page';
 }
 
@@ -20,7 +24,17 @@ function handleHome(event) {
   searchPage.classList.remove('hidden');
   detailsPage.classList.add('hidden');
   resultsPage.classList.add('hidden');
+  favoritesPage.classList.add('hidden');
   data.view = 'home-page';
+}
+
+favoriteLink.addEventListener('click', handleFavorite);
+function handleFavorite(event) {
+  favoritesPage.classList.remove('hidden');
+  searchPage.classList.add('hidden');
+  detailsPage.classList.add('hidden');
+  resultsPage.classList.add('hidden');
+  data.view = 'favorites-page';
 }
 
 formSearch.addEventListener('submit', handleSubmit);
@@ -87,6 +101,7 @@ function handleListClick(event) {
   var type1Char = document.querySelector('.details-type1');
   var type2Char = document.querySelector('.details-type2');
   var imgShinyChar = document.querySelector('.image-shiny');
+  var detailRow = document.querySelector('.detail-row');
 
   for (let i = 0; i < data.entries.length; i++) {
     if (Number(liCharacter.getAttribute('data-character-id')) === data.entries[i].charID) {
@@ -96,12 +111,53 @@ function handleListClick(event) {
       type2Char.textContent = 'Type 2: ' + data.entries[i].type2;
       imgShinyChar.setAttribute('src', data.entries[i].shinyImage);
       imgChar.setAttribute('src', data.entries[i].image);
+      detailRow.setAttribute('data-character-id', data.entries[i].charID);
 
     }
   }
 
   resultsPage.classList.add('hidden');
   searchPage.classList.add('hidden');
+  favoritesPage.classList.add('hidden');
   detailsPage.classList.remove('hidden');
   data.view = 'details-page';
+}
+
+var favList = document.querySelector('#favorites-list');
+
+buttonFav.addEventListener('click', handleAddToFavorites);
+function handleAddToFavorites(event) {
+  var favoriteChar = event.target.closest('[data-character-id]');
+
+  for (let i = 0; i < data.entries.length; i++) {
+    if (Number(favoriteChar.getAttribute('data-character-id')) === data.entries[i].charID) {
+      data.favorite.push(data.entries[i]);
+    }
+  }
+
+  favList.appendChild(getFavoriteList(data.favorite[data.favorite.length - 1]));
+
+  resultsPage.classList.add('hidden');
+  searchPage.classList.add('hidden');
+  detailsPage.classList.add('hidden');
+  favoritesPage.classList.remove('hidden');
+}
+
+function getFavoriteList(dataEntries) {
+  var favoriteItem = document.createElement('div');
+  favoriteItem.setAttribute('class', 'favorite-poke');
+  favoriteItem.setAttribute('data-character-id', dataEntries.charID);
+
+  var favImage = document.createElement('img');
+  favImage.setAttribute('src', dataEntries.image);
+  favImage.setAttribute('class', 'favoriteImage');
+
+  var favName = document.createElement('p');
+  favName.textContent = dataEntries.name.toUpperCase();
+  favName.setAttribute('class', 'name');
+
+  favoriteItem.appendChild(favName);
+  favoriteItem.appendChild(favImage);
+
+  return favoriteItem;
 }
